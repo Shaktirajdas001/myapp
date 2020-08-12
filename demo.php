f25d3d8f7d7aea75b1537b9cb616a55c36a75a93
Shaktirajdas001 <62734281+Shaktirajdas001@users.noreply.github.com>
2020-08-12 08:21:43 +0530

    first commkit

---
+++ demo.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<style type="text/css">
+	.header-img
+	{
+	
+		width:10%;
+		height:10%;
+	}
+	.header
+	{
+		background: gray;
+	}
+	.header h1
+	{
+		text-align: center;
+		margin-top: 20%;
+	}
+	</style>
+</head>
+<body>
+<div class="header"><img src="bput.jpg"><h1 center>Biju Patnaik University Of Technology,Odisha</h1></div>
+</body>
+</html>
